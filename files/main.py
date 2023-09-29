b37a72716c6b0c1a3a253b1c053bcd8e622b9811
Line Le Goff <line.chloe.legoff@gmail.com>
2023-09-29 10:12:21 +0000

    3

--- files/main.py
+++ files/main.py
@@ -8,7 +8,9 @@ server = FastAPI(title='User API')
 
 # creating a connection to the database
 # mysql_url = 'mysql'
-mysql_url = '10.43.11.51'
+# mysql_url = '10.43.11.51'
+# mysql_url = 'sql-api'
+mysql_url = 'localhost'
 mysql_user = os.getenv('user')
 mysql_password = os.getenv('password')
 database_name = 'Main'
